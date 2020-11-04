a2f9b1235471a47ebf01f98fd1998489c286f839
bissuh <bissuh@gmail.com>
2020-11-04 10:15:42 -0300

    Includes message for report not found. Fix button style for loading.

--- src/monthlyReportSearch/Main.tsx
+++ src/monthlyReportSearch/Main.tsx
@@ -19,6 +19,7 @@ const Main = () => {
   const [loading, setLoading] = useState(false);
   const [year, setYear] = useState(currentYear);
   const [month, setMonth] = useState(currentMonth);
+  const [openedModal, setOpenedModal] = useState(false);
 
   const valueTrail = Array.from(
     { length: currentYear - 2000 },
@@ -27,13 +28,21 @@ const Main = () => {
 
   const search = async () => {
     setLoading(true);
-    let selectedMonth = months.findIndex((item) => item === month);
-    if (selectedMonth >= 0) selectedMonth += 1;
 
-    const result = await service?.getMonthlyReport(`${year}-${selectedMonth}`);
-
-    window.open(result?.data.path, '_blank');
-    setLoading(false);
+    try {
+      let selectedMonth = months.findIndex((item) => item === month);
+      console.log(months, month, selectedMonth);
+      if (selectedMonth >= 0) selectedMonth += 1;
+      const result = await service?.getMonthlyReport(
+        `${year}-${selectedMonth}`
+      );
+      window.open(result?.data.path, '_blank');
+    } catch (e) {
+      console.log(e);
+      setOpenedModal(true);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const months = [
@@ -69,8 +78,8 @@ const Main = () => {
           id="month"
           onChange={(evt: any) => setMonth(evt.target.value)}
         >
-          {months.map((value, idx) => (
-            <option value={idx} selected={value === currentMonth}>
+          {months.map((value) => (
+            <option value={value} selected={value === currentMonth}>
               <Text id={value}>{value}</Text>
             </option>
           ))}
@@ -81,16 +90,36 @@ const Main = () => {
           onClick={search}
           style={{ backgroundColor: config.buttonColor }}
         >
-          {loading && (
-            <img
-              src="https://ticker-assets.s3.amazonaws.com/images/loading.svg"
-              style={{ margin: 0, marginRight: '10px' }}
-              width={14}
-            />
+          {loading ? (
+            <Text id="loading">loading</Text>
+          ) : (
+            <Text id="view">view</Text>
           )}
-          <Text id="view">view</Text>
         </button>
       </Spacer>
+
+      {openedModal && (
+        <div>
+          <div
+            className={style.abaModal}
+            onClick={() => setOpenedModal(false)}
+          ></div>
+          <div className={style.abaModalContent}>
+            <Text id="report-not-available">
+              <span className={style.level2}>
+                It seems that this report is not available yet.
+              </span>
+            </Text>
+            <span
+              onClick={() => setOpenedModal(false)}
+              className={style.closeButton}
+              style={{ color: config.buttonColor }}
+            >
+              <Text id="close">close</Text>
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
