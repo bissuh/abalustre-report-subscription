a6296ca2d8d538f34b75bc42526905a3c51ea7ec
Eduardo Lopes <eduardo.lopes@cubos.io>
2021-05-13 16:18:33 -0300

    feat(poolTable): change info icon and make it customizable

--- src/poolTable/Main.tsx
+++ src/poolTable/Main.tsx
@@ -48,6 +48,9 @@ const DEFAULT_STYLES = {
   'last-column': {
     textTransform: 'capitalize',
   },
+  'info-icon': {
+    cursor: 'pointer',
+  },
 };
 
 const Main = () => {
@@ -138,19 +141,23 @@ const Main = () => {
             <td style={finalStyle('table-tbody-td')}>
               {pool.start_date && (
                 <div className={styles.tooltipContainer}>
-                  <img
-                    style={{
-                      ...finalStyle('table-tbody-td-img'),
-                      cursor: 'pointer',
-                    }}
-                    src="https://abalustre-assets.s3.amazonaws.com/information.png"
-                    width={15}
+                  <svg
+                    style={finalStyle('info-icon')}
+                    stroke-width="0"
+                    viewBox="0 0 16 16"
+                    height="1em"
+                    xmlns="http://www.w3.org/2000/svg"
                     className={styles.tooltipTrigger}
-                  />
-                  <div
-                    className={styles.tooltip}
-                    style={finalStyle('table-tbody-td-tooltip')}
                   >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z"
+                      clip-rule="evenodd"
+                    />
+                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z" />
+                    <circle cx="8" cy="4.5" r="1" />
+                  </svg>
+                  <div className={styles.tooltip} style={finalStyle('tooltip')}>
                     <p>
                       <b>{pool.fullname}</b>
                     </p>
